c3f1d8b5a316251fa665840d8fb7b8209aff8a95
JasebM <jmeza792@gmail.com>
2017-02-16 05:35:34 -0400

    Algunos cambios y mejoras al MainPage

--- resources/views/headers/home.blade.php
+++ resources/views/headers/home.blade.php
@@ -12,7 +12,7 @@
         <a class="navbar-brand" href="#"><img class="img-responsive" src="{{url('/assets/img/Logo.png')}}" alt="Migohood Logo">
         </a>
       </div>
-      <div id="navbar3" class="navbar-collapse collapse">
+      <div id="navbar3" class="navbar-collapse collapse text-center">
         <ul class="nav navbar-nav navbar-right">
           <li><a class="nav-link" href="#">Home</a></li>
           <li><a class="nav-link" href="#">Category</a></li>
@@ -23,7 +23,7 @@
           </li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true">USD<span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
+            <ul class="dropdown-menu text-center" role="menu">
               <li><a href="#">COP</a></li>
               <li><a href="#">VEF</a></li>
             </ul>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,22 +4,23 @@
 <head>
     <title></title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">  
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{url('/assets/css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{url('/assets/css/font-awesome.min.css')}}'" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,700" rel="stylesheet">
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="{{url('/assets/css/styles.css')}}" rel="stylesheet">
-    
-    
 </head>
 
 <body>
-    @include ("headers/home") 
-    @yield ('content')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    @include ("headers/home") @yield ('content')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="{{url('/assets/js/jquery.min.js')}}"></script>
     <script src="{{url('/assets/js/bootstrap.min.js')}}'"></script>
 </body>
+<footer>
+    @include ("footers/footer")
+</footer>
 
 </html>
