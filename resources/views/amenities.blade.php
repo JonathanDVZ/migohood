a15fe50ee0cbc9d59ec95b17737ab1f127c591f9
JasebM <jmeza792@gmail.com>
2017-02-27 02:35:44 -0400

    Faltan cosas

--- resources/views/amenities.blade.php
+++ resources/views/amenities.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.master') @section('title', 'Amenities') @section('content')
 <div class="container amenities">
     <div class="row">
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 insideAm">
             <h2 class="text-left">AMENITIES</h2>
             <div class="form-group">
-                <input type="text" placeholder="Amenities 1" class="form-control" id="">
+                <input type="text" placeholder="Amenities 1" class="form-control formis" id="">
             </div>
             <div class="form-group">
-                <input type="text" class="form-control" placeholder="Amenities 2" id="">
+                <input type="text" class="form-control formis" placeholder="Amenities 2" id="">
             </div>
             <div class="form-group">
-                <input type="text" class="form-control" placeholder="Amenities 3" id="">
+                <input type="text" class="form-control formis" placeholder="Amenities 3" id="">
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <button type="submit" class="btn btn-default continue">CONTINUE</button>
         </div>
-        <div class="col-lg-10 col-md-10 col-sm-10"></div>
-
-    </div>
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12"></div>
+</div>
 
 </div>
 @endsection @section("footer") @endsection
