b92a3b68ab3ff2a6e921c29c369715a4e0d3c0dd
JonathanDVZ <jodaviza@gmail.com>
2017-03-22 12:58:50 -0400

    Bootstrap select

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,8 +11,8 @@
     <link rel="shortcut icon" href="{{url('/assets/img/favicon.ico')}}">
     <link href="{{url('/assets/css/sweetalert.css')}}" rel="stylesheet">
     <link href="{{url('/assets/css/bootstrap-datetimepicker.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{url('/assets/css/bootstrap-select.min.css')}}">
     <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,700" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css">
 </head>
 <body class='@yield("class")'>
     @if(Auth::check()) 
@@ -31,8 +31,8 @@
     <script src="{{url('/assets/js/moment.js')}}"></script>
     <script src="{{url('/assets/js/bootstrap.min.js')}}"></script>
     <script src="{{url('/assets/js/bootstrap-datetimepicker.js')}}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/i18n/defaults-*.min.js"></script>
+    <script src="{{url('/assets/js//bootstrap-select.min.js')}}"></script>
+    <!-- <script src="{{url('/assets/js/i18n/defaults-*.min.js')}}"></script> -->
     <script src="{{url('/assets/js/init.js')}}"></script>
 </body>
 
