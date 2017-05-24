a4b610696b4d95dcb168849a95439342250d1165
Jaseb Meza <jmeza792@gmail.com>
2017-05-24 17:02:35 -0400

    Preview

--- resources/views/CreateSpace/PreviewSpace/preview4.blade.php
+++ resources/views/CreateSpace/PreviewSpace/preview4.blade.php
@@ -100,9 +100,74 @@
     @include('CreateSpace.PreviewSpace.navbar.preview-navbar',['activo2' => 'preview4'])
     <div class="container-fluid WhiteBack preBody">
         <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 light-border">
-            <h3>The Neigborhood</h3>
-            <span>Username's place is lcoated in Barcelona, Spain</span>
+            <div style="margin: 1em;">
+                <h3 style="color:#0093b4">The Neigborhood</h3>
+                <span>Username's </span> <span>place is lcoated in</span><span> Barcelona, Spain</span>
+            </div>
+
             <div id="googleMap" style="width:95%;height:250px;margin:1.5em;"></div>
+            <hr style="color:#ccc; margin: auto 1em;">
+            <h5 style="color:#0093b4; margin: 1em;">Similar Listing</h5>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div id="carousel" class="carousel slide lilcarrousel" data-ride="carousel">
+                    <div class="carousel-inner carrousel2">
+
+                        <div class="item active">
+                            <img class="img-responsive img2" src="{{url('/assets/img/fondofs1.png')}}" alt="Slide 1" />
+                        </div>
+                        <div class="item">
+                            <img class="img-responsive img2" src="{{url('/assets/img/fondofs1.png')}}" alt="Slide 2" />
+                        </div>
+                        <div class="item">
+                            <img class="img-responsive img2" src="{{url('/assets/img/fondofs1.png')}}" alt="Slide 3" />
+                        </div>
+                    </div>
+                    <a href="#carousel" class="left carousel-control" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                    </a>
+                    <a href="#carousel" class="right carousel-control" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                    </a>
+                </div>
+                <div>
+                    <h5 class="RetNex2" style="color:#0093b4; margin: 1em;">Warm Bedroom</h5>
+                    <span class="RetNex2"><strong>5$</strong></span>
+                    <br>
+                    <span>Private Room |</span>
+                    <span>2 Beds |</span>
+                    <span>2 Guest</span>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div id="carousel" class="carousel slide lilcarrousel" data-ride="carousel">
+                    <div class="carousel-inner carrousel2">
+
+                        <div class="item active">
+                            <img class="img-responsive img2" src="{{url('/assets/img/fondofs1.png')}}" alt="Slide 1" />
+                        </div>
+                        <div class="item">
+                            <img class="img-responsive img2" src="{{url('/assets/img/fondofs1.png')}}" alt="Slide 2" />
+                        </div>
+                        <div class="item">
+                            <img class="img-responsive img2" src="{{url('/assets/img/fondofs1.png')}}" alt="Slide 3" />
+                        </div>
+                    </div>
+                    <a href="#carousel" class="left carousel-control" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                    </a>
+                    <a href="#carousel" class="right carousel-control" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                    </a>
+                </div>
+                <div>
+                    <h5 class="RetNex2" style="color:#0093b4; margin: 1em;">Warm Bedroom</h5>
+                    <span class="RetNex2"><strong>5$</strong></span>
+                    <br>
+                    <span>Private Room |</span>
+                    <span>2 Beds |</span>
+                    <span>2 Guest</span>
+                </div>
+            </div>
 
         </div>
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 light-border">
